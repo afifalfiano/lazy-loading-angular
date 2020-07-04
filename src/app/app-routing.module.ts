78769db9e7b3530e66d10c64753cb4fc1b38e861
Afif Alfiano <afif.alfiano@uii.ac.id>
2020-07-04 15:20:48 +0700

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) },
+  { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
